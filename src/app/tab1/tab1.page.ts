9597d9beb1c9a9a2581ac0e8cc8873e85b307445
Harish Majety <harish.majety@eRT.com>
2021-01-08 08:08:09 +0530

    delete aray

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,19 +1,25 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  selector: "app-tab1",
+  templateUrl: "tab1.page.html",
+  styleUrls: ["tab1.page.scss"],
 })
 export class Tab1Page {
-
-  contacts :any;
+  contacts: any;
   constructor() {
-    this.contacts = [{"name":"harish","num":"32423423424"},{"name":"harish","num":"32423423424"},{"name":"harish","num":"32423423424"},{"name":"harish","num":"32423423424"},{"name":"harish","num":"32423423424"},{"name":"harish","num":"32423423424"}]
-  }
-
-  deleteRow(){
     
+    this.contacts = [
+      { name: "harish", num: "32423423424" },
+      { name: "Soundarya", num: "134354546" },
+      { name: "Anirudh", num: "898989" },
+      { name: "Srita", num: "5656565" },
+      { name: "Dhoni", num: "465465" },
+      { name: "Jadeja", num: "8988" },
+    ];
   }
 
+  deleteContact(index) {
+    this.contacts.splice(index,1)
+  }
 }
